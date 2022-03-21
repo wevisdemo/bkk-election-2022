5af89467123b14d90744f377dd0618d753f0e574
Th1nkK1D <Th1nkK1D@users.noreply.github.com>
2022-03-21 12:17:00 +0000

    deploy: eb74c256da0b5743ada48be6f8bb7556ce966f47

---
+++ candidate/_next/static/67h2urxvpQojVpcWIGaRC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fd2ed048b9955b88.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
