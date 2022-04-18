93380270787a1ea1e6377acd94ab366d487c32c0
Th1nkK1D <witheep@gmail.com>
2022-04-18 13:40:49 +0300

    Add sharer ui to landing

--- apps/landing/src/routes/__layout.svelte
+++ apps/landing/src/routes/__layout.svelte
@@ -11,5 +11,10 @@
 <div>
 	<ui-navbar />
 	<slot />
+	<div class="bg-black px-3">
+		<div class="max-w-screen-xl mx-auto flex justify-center py-8 border-white border-b">
+			<ui-sharer url="https://bkkelection2022.wevis.info" />
+		</div>
+	</div>
 	<ui-footer />
 </div>
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,8 +2,11 @@ import { customElement } from 'solid-element';
 import Navbar from './navbar';
 import Footer from './footer';
 import PostCard, { postCardDefaultProps } from './post-card';
+import Sharer, { sharerDefaultProps } from './sharer';
+
 import '../style.css';
 
 customElement('ui-navbar', Navbar);
 customElement('ui-footer', Footer);
 customElement('ui-post-card', postCardDefaultProps, PostCard);
+customElement('ui-sharer', sharerDefaultProps, Sharer);
