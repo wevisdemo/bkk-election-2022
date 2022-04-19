7ffab36b98055b901fd71e24242eee30d1b78bc9
Th1nkK1D <witheep@gmail.com>
2022-04-19 12:47:51 +0300

    Update ui preview page

--- packages/ui/src/preview.tsx
+++ packages/ui/src/preview.tsx
@@ -1,15 +1,9 @@
 /* @refresh reload */
-import { Component, onMount } from 'solid-js';
+import { Component } from 'solid-js';
 import { render } from 'solid-js/web';
-
 import './components/index';
 
 const App: Component = () => {
-  onMount(() => {
-    // @ts-ignore
-    window.registerUICustomElements();
-  });
-
   return (
     <div
       style={{
@@ -19,14 +13,21 @@ const App: Component = () => {
       }}
     >
       <ui-navbar></ui-navbar>
-      <div style={{ flex: 1, padding: '20px' }}>
-        <h1 className="typo-h3">UI Library</h1>
+      <div style={{ padding: '20px', width: '400px' }}>
         <ui-post-card
           title="มองย้อนการเลือกตั้งผู้ว่าฯ กทม. 9 ปี ศึกชิงเมืองกรุงภายใต้การเมือง 2 พรรคใหญ่ ฝ่ายค้าน vs. รัฐบาล"
           link="https://thestandard.co/9-years-history-of-bangkok-governor-election/"
           date="28 มีนาคม 2565"
           image="https://thestandard.co/wp-content/uploads/2022/03/COVER-WEB-9-33-400x215.jpg"
         ></ui-post-card>
+      </div>
+      <div
+        style={{
+          'background-color': 'gray',
+          padding: '20px',
+        }}
+      >
+        <ui-sharer url="test"></ui-sharer>
       </div>
       <ui-footer></ui-footer>
     </div>
