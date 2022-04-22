1bbc05a9e470f654c4bb77282761c0ba375daefc
Th1nkK1D <Th1nkK1D@users.noreply.github.com>
2022-04-22 08:39:41 +0000

    deploy: 7b02bc5d960d5dd7b987186c39e5ac53fd0bfd4b

--- socialtrend/_nuxt/static/1650549186/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsocialtrend\u002F_nuxt\u002Fstatic\u002F1650549186",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsocialtrend\u002F",assetsPath:"\u002Fsocialtrend\u002F_nuxt\u002F",cdnURL:a}}}}(null));
