21db96a707afb36c739080984a7094f0aacd2ff0
Th1nkK1D <witheep@gmail.com>
2022-04-25 15:46:46 +0300

    feat(map): Update candidate model

--- apps/map/src/models/candidate.ts
+++ apps/map/src/models/candidate.ts
@@ -1,6 +1,8 @@
 export interface Candidate {
+	id: string; // ผู้ว่า = number, ส.ก. = district-number
 	number: string;
-	name: string;
+	fullname: string;
+	shortname: string;
 	color: string;
 	party: string;
 	image: string;
