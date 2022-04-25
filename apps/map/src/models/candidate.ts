acc09d001061266abb170231b2c2fc43a9453468
Th1nkK1D <witheep@gmail.com>
2022-04-25 17:20:48 +0300

    feat(map): Add 2565 election and candudate data

--- apps/map/src/models/candidate.ts
+++ apps/map/src/models/candidate.ts
@@ -1,11 +1,11 @@
 export interface Candidate {
 	id: string; // ผู้ว่า = number, ส.ก. = district-number
-	number: string;
+	number?: number;
 	fullname: string;
 	shortname: string;
 	color: string;
-	party: string;
-	image: string;
+	party?: string;
+	image?: string;
 }
 
 export type CandidateMap = {
