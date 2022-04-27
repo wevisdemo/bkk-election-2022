6fc7e22d83b4105459172a66c7e1991230295650
Th1nkK1D <witheep@gmail.com>
2022-04-27 16:32:47 +0300

    map(feat): Organize dashboard layout

--- apps/map/src/components/dashboard.tsx
+++ apps/map/src/components/dashboard.tsx
@@ -17,13 +17,24 @@ const Dashboard: FunctionComponent = () => {
 		.map((v: Result) => preset.candidateMap[v.candidateId]);
 
 	return (
-		<div className="bg-black text-white flex-1 p-12">
-			<h1 className="typo-h2">{preset.fullname}</h1>
-			<CandidateOverviewList />
-			<CandidateLegen candidates={candidates}>
-				ขนาดกล่อง ตามจำนวนผู้มีสิทธิ์เลือกตั้งในเขตนั้น <br />
-				สัดส่วนสี ในแต่ละกล่องตามสัดส่วนคะแนนของผู้สมัคร
-			</CandidateLegen>
+		<div className="bg-black text-white flex-1 p-12 space-y-12">
+			<div className="border-b pb-6 border-gray">
+				<h1 className="typo-h2">{preset.fullname}</h1>
+			</div>
+			<div class="flex flex-row space-x-12">
+				<div className="flex-1 space-y-6">
+					<h2 className="typo-h4">คะแนนรวมทั้ง กทม.</h2>
+					<CandidateOverviewList />
+				</div>
+				<div className="w-2/3">
+					<h2 className="typo-h4">คะแนนรายเขต</h2>
+
+					<CandidateLegen candidates={candidates}>
+						ขนาดกล่อง ตามจำนวนผู้มีสิทธิ์เลือกตั้งในเขตนั้น <br />
+						สัดส่วนสี ในแต่ละกล่องตามสัดส่วนคะแนนของผู้สมัคร
+					</CandidateLegen>
+				</div>
+			</div>
 		</div>
 	);
 };
