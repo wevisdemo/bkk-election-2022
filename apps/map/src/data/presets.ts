e6a334c547fcd2b1890a8974977400c386495827
mixth <me@mixth.net>
2022-05-09 01:31:34 +0700

    feat(map): Update freshly fetched candidates and electiondata jsons from ERS

--- apps/map/src/data/presets.ts
+++ apps/map/src/data/presets.ts
@@ -4,8 +4,8 @@ export const electionIndexes: ElectionIndex[] = [
 	{
 		shortname: `ผลผู้ว่าฯ '65 (เร็วๆ นี้)`,
 		fullname: `ผลเลือกตั้งผู้ว่าฯ กทม. '65`,
-		electionDataUrl: '',
-		candidateDataUrl: ''
+		electionDataUrl: '/map/data/65-electiondata.json',
+		candidateDataUrl: '/map/data/65-candidates.json'
 	},
 	{
 		shortname: `Exit Poll ผู้ว่าฯ '65`,
@@ -21,8 +21,8 @@ export const electionIndexes: ElectionIndex[] = [
 	},
 	{
 		shortname: `คะแนนย้อนหลัง '56`,
-		fullname: `ผลเลือกตั้งผู้ว่าฯ กทม. '65`,
-		electionDataUrl: '/map/data/65-electiondata.json',
-		candidateDataUrl: '/map/data/65-candidates.json'
+		fullname: `ผลเลือกตั้งผู้ว่าฯ กทม. '56`,
+		electionDataUrl: '/map/data/56-electiondata.json',
+		candidateDataUrl: '/map/data/56-candidates.json'
 	}
 ];
