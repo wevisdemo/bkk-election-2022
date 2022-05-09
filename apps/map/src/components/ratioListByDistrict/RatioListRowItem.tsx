40825910e0533207ff448bd26da6e9f99a0aa155
Th1nkK1D <witheep@gmail.com>
2022-05-09 14:21:50 +0300

    fix(map): Use 0-100 progress and at lastUpdatedAt in mock live data

--- apps/map/src/components/ratioListByDistrict/RatioListRowItem.tsx
+++ apps/map/src/components/ratioListByDistrict/RatioListRowItem.tsx
@@ -18,46 +18,53 @@ export default function RatioListRowItem({
 
 	if (!preset) return <></>;
 
-	const countingProgress: number = district.voting.progress || 1;
+	const countingProgress: number = district.voting.progress || 100;
 	const countingProgressItems: ProgressItem[] = [
 		{
 			percent: countingProgress,
 			color: '#FFFFFF',
 			strip: isLive
 		},
-		{ percent: 1 - countingProgress,
-			color: 'rgba(255, 255, 255, 0.2)'
-		}
+		{ percent: 1 - countingProgress, color: 'rgba(255, 255, 255, 0.2)' }
 	];
 
-	const progressItems: ProgressItem[] = useMemo(() => district.voting.result
-		.sort((a: Result, b: Result) => b.count - a.count)
-		.map((res: Result) => {
-			return {
-				percent: res.count / district.voting.totalVotes,
-				color: preset.candidateMap[res.candidateId].color
-			};
-		}), [district]);
+	const progressItems: ProgressItem[] = useMemo(
+		() =>
+			district.voting.result
+				.sort((a: Result, b: Result) => b.count - a.count)
+				.map((res: Result) => {
+					return {
+						percent: res.count / district.voting.totalVotes,
+						color: preset.candidateMap[res.candidateId].color
+					};
+				}),
+		[district]
+	);
 
 	return (
-		<div class={`grid ${isInProgress ? 'grid-cols-3 md:grid-cols-6' : 'grid-cols-2 md:grid-cols-5'} typo-u4 gap-x-4 gap-y-1 md:gap-8 hover:bg-white/20`}>
+		<div
+			class={`grid ${
+				isInProgress ? 'grid-cols-3 md:grid-cols-6' : 'grid-cols-2 md:grid-cols-5'
+			} typo-u4 gap-x-4 gap-y-1 md:gap-8 hover:bg-white/20`}
+		>
 			<div class="font-semibold">{district.name}</div>
-			<div class={ isInProgress ? 'text-left' : 'text-right md:text-left'}>
+			<div class={isInProgress ? 'text-left' : 'text-right md:text-left'}>
 				{district.voting.eligiblePopulation.toLocaleString()} (
 				{(
 					district.voting.eligiblePopulation / preset.electionData.total.eligiblePopulation
 				).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 1 })}
 				)
 			</div>
-			<div class={`${isInProgress ? 'col-span-3' : 'col-span-2 md:col-span-3'} flex grow order-last md:order-3 my-auto`}>
+			<div
+				class={`${
+					isInProgress ? 'col-span-3' : 'col-span-2 md:col-span-3'
+				} flex grow order-last md:order-3 my-auto`}
+			>
 				<Progress border="1px solid #000000" sClass="h-[10px]" progressItems={progressItems} />
 			</div>
 			{isInProgress && (
 				<div class="flex md:basis-2/12 gap-2 order-4 my-auto">
-					{countingProgress.toLocaleString(undefined, {
-						style: 'percent',
-						minimumFractionDigits: 1
-					})}
+					{countingProgress.toFixed(1)}%
 					<Progress progressItems={countingProgressItems} sClass="h-1 md:h-2" />
 				</div>
 			)}
