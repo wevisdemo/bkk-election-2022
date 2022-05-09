0e2603aa7e555d64aeae15381eeebf83187a072a
Th1nkK1D <witheep@gmail.com>
2022-05-09 12:14:55 +0300

    fix(map): Move images from static to public folder

--- apps/map/src/components/Progress.tsx
+++ apps/map/src/components/Progress.tsx
@@ -11,18 +11,18 @@ export interface ProgressItem {
 	strip?: boolean;
 }
 
-const STRIP_GIF_PATH = '/map/static/images/strip-black.gif';
+const STRIP_GIF_PATH = '/map/images/strip-black.gif';
 
 export default function Progress({ progressItems, border, sClass, children }: ProgressProps) {
 	return (
-		<div class='inline-flex w-full h-full my-auto'>
+		<div class="inline-flex w-full h-full my-auto">
 			{progressItems.map((pi: ProgressItem) => (
 				<span
 					class={`h-full ${sClass}`}
 					style={{
-						width: `${pi.percent*100}%`,
+						width: `${pi.percent * 100}%`,
 						backgroundColor: pi.color,
-						border: border,
+						border: border
 					}}
 				>
 					{pi.strip && (
