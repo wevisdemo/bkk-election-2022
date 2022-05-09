5b77468cda18dbeff0d763bdfa983ea041458d8e
Tae Prasongpongchai <taepras@gmail.com>
2022-05-10 00:49:32 +0700

    minor tweaks for districttooltip to ignore mouse so we can glide smoothly

--- apps/map/src/components/DistrictTooltip.tsx
+++ apps/map/src/components/DistrictTooltip.tsx
@@ -33,7 +33,7 @@ const DistrictTooltip: FunctionComponent<DistrictTooltipProps> = ({
 	return (
 		<div
 			id="district-tooltip"
-			className={`absolute z-10 min-w-[10rem] shadow ${className}`}
+			className={`absolute z-10 min-w-[10rem] shadow pointer-events-none ${className}`}
 			style={style}
 		>
 			<div>
@@ -44,11 +44,11 @@ const DistrictTooltip: FunctionComponent<DistrictTooltipProps> = ({
 			<div className="bg-[#393939] py-2 px-3 typo-u4 space-y-2 rounded-sm">
 				<div>{district.name}</div>
 				<div className="space-y-1">
-					{displayCandidate.map(({ shortname, number, color, percentage }) => (
-						<div className="flex flex-row items-center space-x-2 font-bold">
+					{displayCandidate.map(({ shortname, number, color, percentage }, index) => (
+						<div className={`flex flex-row items-center space-x-2 ${index === 0 ? 'font-bold' : ''}`}>
 							<div className="w-2 h-2" style={{ backgroundColor: color }} />
 							<div className="flex-1">
-								[{number}] {shortname}
+								{number ? `[${number}] ` : ''}{shortname}
 							</div>
 							<div>{percentage}%</div>
 						</div>
