1ebcd88d948f5ae3c07324f4631b324caa904898
Tae Prasongpongchai <taepras@gmail.com>
2022-05-14 01:36:10 +0700

    make header functional

---
+++ apps/map/src/components/HeaderPresetToggle.tsx
@@ -0,0 +1,85 @@
+import React, { FunctionComponent, useContext, useState } from 'react';
+import { presetContext } from '../contexts/preset';
+import { electionIndexes } from '../data/presets';
+import { ElectionDataType } from '../models/election';
+
+interface PresetToggleProps {
+	activeIndex: number;
+	onChange: (e: number) => void;
+}
+
+const HeaderPresetToggle: FunctionComponent<PresetToggleProps> = ({ activeIndex, onChange }) => {
+	const preset = useContext(presetContext);
+	const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+	return (
+		<div className="w-full lg:w-full flex flex-col relative">
+			<div className="flex-1 text-center">
+				<h1 className=" typo-h2 text-[24px] lg:text-[48px]">{preset?.electionData?.type === ElectionDataType.Live && <LiveBadge />} {preset?.fullname}</h1>
+				<p className="typo-u4 lg:text-[16px] mt-2 lg:mt-1">
+					{preset?.subtitle}
+					<button className='px-1 bg-[#ccc] m-1 text-black rounded typo-u4 font-[600]' onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+						เปลี่ยนชุดข้อมูล
+						<svg
+							width="14"
+							height="9"
+							viewBox="0 0 14 9"
+							fill="none"
+							className={`inline-block transform transition-transform duration-150`}
+						>
+							<path d="M1 1L7 7L13 1" stroke="black" stroke-width="2" />
+						</svg>
+					</button>
+				</p>
+			</div>
+			{/* <button
+					class="flex flex-row items-center rounded-sm border border-white py-2 px-4 mb-1 typo-u5 font-bold"
+					onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+				>
+					<div className="flex-1 text-left flex flex-row">
+						{preset?.electionData.type === ElectionDataType.Live && <LiveBadge />}
+						{electionIndexes[activeIndex].shortname}
+					</div>
+					<svg
+						width="14"
+						height="9"
+						viewBox="0 0 14 9"
+						fill="none"
+						className={`transform transition-transform duration-150 ${isDropdownOpen ? 'rotate-180' : ''
+							}`}
+					>
+						<path d="M1 1L7 7L13 1" stroke="white" stroke-width="2" />
+					</svg>
+				</button> */}
+			<div
+				className={`absolute max-w-[360px] m-auto inset-x-0 top-full flex-col rounded-sm border border-white bg-black z-10 overflow-hidden ${isDropdownOpen ? 'flex' : 'hidden'
+					}`}
+			>
+				{electionIndexes.map(({ shortname, electionDataUrl }, index) => (
+					<button
+						key={shortname}
+						disabled={!electionDataUrl}
+						onClick={() => {
+							setIsDropdownOpen(false)
+							if (electionDataUrl) onChange(index)
+						}}
+						className={`typo-u5 px-3 py-2 rounded-sm h-fit m-[2px] flex flex-row ${index === activeIndex
+							? 'bg-white text-black font-semibold'
+							: electionDataUrl
+								? 'hover:bg-white hover:text-black'
+								: 'opacity-40'
+							}`}
+					>
+						{preset?.electionData.type === ElectionDataType.Live &&
+							preset?.shortname === shortname && <LiveBadge />}
+						{shortname}
+					</button>
+				))}
+			</div>
+		</div>
+	);
+};
+
+const LiveBadge = () => <div class="bg-[#D02525] typo-u4 text-white mr-1 px-1 font-semibold inline-block rounded-[2px] align-middle">LIVE</div>;
+
+export default HeaderPresetToggle;
--- apps/map/src/components/dashboard.tsx
+++ apps/map/src/components/dashboard.tsx
@@ -8,6 +8,7 @@ import TabsView from './TabsView';
 import DistrictVisualization from './DistrictVisualization';
 import { Visualization } from '../models/visualization';
 import { ElectionDataType } from '../models/election';
+import HeaderPresetToggle from './HeaderPresetToggle';
 interface DashboardProps {
 	activePresetIndex: number;
 	onPresetChange: (i: number) => void;
@@ -22,18 +23,8 @@ const Dashboard: FunctionComponent<DashboardProps> = ({ activePresetIndex, onPre
 	return (
 		<div className="flex-1 flex flex-col bg-black text-white px-5 pt-4 pb-2 lg:px-12 lg:py-8 space-y-1 overflow-hidden lg:space-y-6 pb-16">
 			<div className="flex flex-col lg:flex-row gap-3 md:gap-4 lg:border-b lg:pb-6 border-gray items-center mb-2">
-				<div className="flex-1 text-center">
-					<h1 className=" typo-h2 text-[24px] lg:text-[48px]">{preset?.electionData?.type === ElectionDataType.Live && <LiveBadge />} {preset.fullname}</h1>
-					<p className="typo-u4 lg:text-[16px] mt-2 lg:mt-1">{preset.subtitle} <a href="#" className='px-1 bg-[#999] m-1 text-black rounded'>เปลี่ยนชุดข้อมูล <svg
-						width="14"
-						height="9"
-						viewBox="0 0 14 9"
-						fill="none"
-						className={`inline-block transform transition-transform duration-150`}
-					>
-						<path d="M1 1L7 7L13 1" stroke="black" stroke-width="2" />
-					</svg></a></p>
-				</div>
+				
+				<HeaderPresetToggle activeIndex={activePresetIndex} onChange={onPresetChange} />
 
 				{/* <PresetToggle activeIndex={activePresetIndex} onChange={onPresetChange} /> */}
 			</div>
@@ -63,8 +54,5 @@ const Dashboard: FunctionComponent<DashboardProps> = ({ activePresetIndex, onPre
 		</div>
 	);
 };
-
-const LiveBadge = () => <div class="bg-[#D02525] typo-u4 text-white mr-1 px-1 font-semibold inline-block rounded-[2px] align-middle">LIVE</div>;
-
 
 export default Dashboard;
