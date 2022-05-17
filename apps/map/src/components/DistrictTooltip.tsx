53188968226afda6dc409812024665b4684f1f0a
Th1nkK1D <witheep@gmail.com>
2022-05-17 16:09:19 +0300

    fix(map): Display 0 instead of NaN when count is 0 in tooltip

--- apps/map/src/components/DistrictTooltip.tsx
+++ apps/map/src/components/DistrictTooltip.tsx
@@ -26,13 +26,15 @@ const DistrictTooltip: FunctionComponent<DistrictTooltipProps> = ({
 
 	if (!show || !preset) return <></>;
 
-	const displayCandidate = district ? [...district.voting.result]
-		.sort((a, z) => z.count - a.count)
-		.slice(0, topCandidateDisplay)
-		.map(({ candidateId, count }) => ({
-			...preset.candidateMap[candidateId],
-			percentage: ((count / district.voting.totalVotes) * 100).toFixed(1)
-		})) : [];
+	const displayCandidate = district
+		? [...district.voting.result]
+				.sort((a, z) => z.count - a.count)
+				.slice(0, topCandidateDisplay)
+				.map(({ candidateId, count }) => ({
+					...preset.candidateMap[candidateId],
+					percentage: count > 0 ? ((count / district.voting.totalVotes) * 100).toFixed(1) : 0
+				}))
+		: [];
 
 	return (
 		<div
@@ -40,36 +42,53 @@ const DistrictTooltip: FunctionComponent<DistrictTooltipProps> = ({
 			className={`absolute z-10 min-w-[10rem] pointer-events-none ${className}`}
 			style={style}
 		>
-			{pointUp && <div>
-				<svg width="12" height="6" viewBox="0 0 12 6" fill="none" className="ml-2">
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M6 0L12 6H0L6 0Z" fill="#393939" />
-				</svg>
-			</div>}
-			<div className={`bg-[#393939] py-2 px-3 typo-u4 space-y-2 rounded-sm shadow`}
+			{pointUp && (
+				<div>
+					<svg width="12" height="6" viewBox="0 0 12 6" fill="none" className="ml-2">
+						<path fill-rule="evenodd" clip-rule="evenodd" d="M6 0L12 6H0L6 0Z" fill="#393939" />
+					</svg>
+				</div>
+			)}
+			<div
+				className={`bg-[#393939] py-2 px-3 typo-u4 space-y-2 rounded-sm shadow`}
 				// style={anchorRight ? { position: "relative", left: "-80%" } : { left: "unset" }}
-				style={anchorRight ? { position: "relative", left: "-80%" } : { position: "relative", left: "unset" }}
+				style={
+					anchorRight
+						? { position: 'relative', left: '-80%' }
+						: { position: 'relative', left: 'unset' }
+				}
 			>
 				<div>{district?.name}</div>
 				<div className="space-y-1">
 					{displayCandidate.map(({ shortname, number, color, percentage }, index) => (
-						<div className={`flex flex-row items-center space-x-2 ${index === 0 ? 'font-bold' : ''}`}>
+						<div
+							className={`flex flex-row items-center space-x-2 ${index === 0 ? 'font-bold' : ''}`}
+						>
 							<div className="w-2 h-2" style={{ backgroundColor: color }} />
 							<div className="flex-1">
-								{number ? `[${number}] ` : ''}{shortname}
+								{number ? `[${number}] ` : ''}
+								{shortname}
 							</div>
 							<div>{percentage}%</div>
 						</div>
 					))}
 				</div>
 			</div>
-			{
-				!pointUp && <div>
-					<svg width="12" height="6" viewBox="0 0 12 6" fill="none" className="ml-2" transform="scale(1,-1)">
+			{!pointUp && (
+				<div>
+					<svg
+						width="12"
+						height="6"
+						viewBox="0 0 12 6"
+						fill="none"
+						className="ml-2"
+						transform="scale(1,-1)"
+					>
 						<path fill-rule="evenodd" clip-rule="evenodd" d="M6 0L12 6H0L6 0Z" fill="#393939" />
 					</svg>
 				</div>
-			}
-		</div >
+			)}
+		</div>
 	);
 };
 
