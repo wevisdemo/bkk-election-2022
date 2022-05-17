8cd834794896f977953c1c0d8b85d14c7119a2b0
Th1nkK1D <witheep@gmail.com>
2022-05-17 13:45:00 +0300

    fix(map): Case insensitive import cause failed build

--- apps/map/src/components/DistrictVisualization.tsx
+++ apps/map/src/components/DistrictVisualization.tsx
@@ -3,11 +3,8 @@ import RatioList from './ratioListByDistrict/RatioList';
 import { Visualization } from '../models/visualization';
 import { presetContext } from '../contexts/preset';
 import VisualizationToggle from './VisualizationToggle';
-// import DistrictMap from './district-map/district-map-canvas';
-import Pixi from './district-map/GridRatio';
 import CandidateLegend from './CandidateLegend';
-import { TOP_CANDIDATE_DISPLAY } from '../constants/candidate';
-import MapWinner from './district-map/MapWInner';
+import MapWinner from './district-map/MapWinner';
 import GridWinner from './district-map/GridWinner';
 import GridRatio from './district-map/GridRatio';
 
@@ -42,10 +39,12 @@ const DistrictVisualization: FunctionComponent<DistrictVisualizationProps> = ({
 							<span className="font-bold">สัดส่วนสี</span> ในแต่ละกล่องตามสัดส่วนคะแนนของผู้สมัคร
 						</p>
 					</div>
-				) : (activeViz === Visualization.GRID_WINNER || activeViz === Visualization.MAP_WINNER) && (
-					<p>
-						<span className="font-bold">สี</span> ของแต่ละเขตแสดงผู้ได้คะแนนสูงสุดในเขตนั้นๆ
-					</p>
+				) : (
+					(activeViz === Visualization.GRID_WINNER || activeViz === Visualization.MAP_WINNER) && (
+						<p>
+							<span className="font-bold">สี</span> ของแต่ละเขตแสดงผู้ได้คะแนนสูงสุดในเขตนั้นๆ
+						</p>
+					)
 				)}
 			</CandidateLegend>
 		);
@@ -56,15 +55,30 @@ const DistrictVisualization: FunctionComponent<DistrictVisualizationProps> = ({
 			className={`flex flex-col md:flex-row w-full h-full gap-3 md:gap-8 overflow-hidden ${className}`}
 		>
 			<div className="flex flex-1 h-full w-full flex-col overflow-y-hidden">
-				<h2 className={`typo-h4 mb-2 md:mb-6 hidden lg:block z-[1] pointer-events-none ${activeViz === Visualization.LIST_RATIO ? '' : 'md:mb-[-40px]'}`}>คะแนนรายเขต</h2>
-				<div className={`${activeViz === Visualization.LIST_RATIO ? 'flex' : ''} flex-col flex-auto h-full overflow-hidden relative`}>
+				<h2
+					className={`typo-h4 mb-2 md:mb-6 hidden lg:block z-[1] pointer-events-none ${
+						activeViz === Visualization.LIST_RATIO ? '' : 'md:mb-[-40px]'
+					}`}
+				>
+					คะแนนรายเขต
+				</h2>
+				<div
+					className={`${
+						activeViz === Visualization.LIST_RATIO ? 'flex' : ''
+					} flex-col flex-auto h-full overflow-hidden relative`}
+				>
 					{activeViz === Visualization.GRID_WINNER && <GridWinner />}
 					{activeViz === Visualization.GRID_RATIO && <GridRatio />}
 					{activeViz === Visualization.MAP_WINNER && <MapWinner />}
 					{activeViz === Visualization.LIST_RATIO && <RatioList />}
-
 				</div>
-				<div class={`md:flex hidden mt-2 ${activeViz === Visualization.LIST_RATIO ? '' : 'md:mt-[-60px]'}`}>{candidateLegend}</div>
+				<div
+					class={`md:flex hidden mt-2 ${
+						activeViz === Visualization.LIST_RATIO ? '' : 'md:mt-[-60px]'
+					}`}
+				>
+					{candidateLegend}
+				</div>
 			</div>
 			<div class="flex md:hidden">{candidateLegend}</div>
 			<div className="flex justify-center items-center">
