f6d5f5b5c661152279cd95be4e16f1abbab0ee1d
Th1nkK1D <witheep@gmail.com>
2022-05-18 20:48:29 +0300

    perf(map): Lazyload CandidateOverviewList

--- apps/map/src/components/dashboard.tsx
+++ apps/map/src/components/dashboard.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, lazy } from 'react';
 import { useContext } from 'react';
 import { FunctionComponent } from 'react';
 import { presetContext } from '../contexts/preset';
-import CandidateOverviewList from './candidateOverviewList/CandidateOverviewList';
 import TabsView from './TabsView';
 import DistrictVisualization from './DistrictVisualization';
 import { Visualization } from '../models/visualization';
 import HeaderPresetToggle from './HeaderPresetToggle';
+import LazyloadContainer from './LazyloadContainer';
 interface DashboardProps {
 	activePresetIndex: number;
 	onPresetChange: (i: number) => void;
@@ -18,6 +18,8 @@ const Dashboard: FunctionComponent<DashboardProps> = ({ activePresetIndex, onPre
 
 	if (!preset) return <></>;
 
+	const CandidateOverviewList = lazy(() => import('./candidateOverviewList/CandidateOverviewList'));
+
 	return (
 		<div className="flex-1 flex flex-col bg-black text-white px-5 pt-4 pb-2 lg:px-12 lg:py-8 space-y-1 overflow-hidden overflow-auto-shortscreen lg:space-y-6 pb-12 xs:pb-16 lg:pb-8">
 			<div className="flex flex-col lg:flex-row gap-3 md:gap-4 lg:border-b lg:pb-6 border-gray items-center mb-2">
@@ -27,7 +29,12 @@ const Dashboard: FunctionComponent<DashboardProps> = ({ activePresetIndex, onPre
 			<div class="flex-1 hidden lg:flex flex-row space-x-12 overflow-hidden">
 				<div className="flex flex-col flex-1 space-y-6 h-full">
 					<h2 className="typo-h4">คะแนนรวมทั้ง กทม.</h2>
-					<CandidateOverviewList votingData={preset.electionData.total} enableTopHighlight={true} />
+					<LazyloadContainer>
+						<CandidateOverviewList
+							votingData={preset.electionData.total}
+							enableTopHighlight={true}
+						/>
+					</LazyloadContainer>
 				</div>
 				<DistrictVisualization
 					activeViz={activeViz}
@@ -41,7 +48,14 @@ const Dashboard: FunctionComponent<DashboardProps> = ({ activePresetIndex, onPre
 				tabs={[
 					{
 						name: 'คะแนนรวมทั้ง กทม.',
-						component: <CandidateOverviewList votingData={preset.electionData.total} enableTopHighlight={true} />
+						component: (
+							<LazyloadContainer>
+								<CandidateOverviewList
+									votingData={preset.electionData.total}
+									enableTopHighlight={true}
+								/>
+							</LazyloadContainer>
+						)
 					},
 					{
 						name: 'คะแนนรายเขต',
